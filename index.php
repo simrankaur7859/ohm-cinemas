cc65516fc08c4b39689d511e15609cbfa6f6f414
simma1209 <46017085+simma1209@users.noreply.github.com>
2019-05-21 14:28:02 +1000

    home page of admin panel
    Works but need improvement

--- index.php
+++ index.php
@@ -1,56 +1,38 @@
+<html>
+	<head>
+		<title>Admin Panel</title>
+		<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+		<link rel="stylesheet" type="text/css" href="../css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="../css/admin.css">
+	</head>
+<body>
 
-<?php include('header.php'); ?>
-
-	
-
-			<div class="col-md-12 banner" style="margin-top:0px;">
-				<!-- <div class="col-md-4 col-md-offset-8 detail" >
-						<h2>BIG HERO 6</h2>
-						<p><span>Genre   </span>: Animation, Action, Comedy</p>
-						<p><span>Release </span>: 7 November 2014</p>
-
-						<p>The special bond that develops between plus-sized inflatable robot Baymax, and prodigy Hiro Hamada, who team up with a group of friends to form a band of high-tech heroes.</p>
-
-						<a href="https://www.youtube.com/watch?v=z3biFxZIJOQ" target="_blank" class="button shade"><i class="fa fa-youtube-play fa-2x fa-fw"></i><span style="width:150px; margin-left:10px;display:inline-block">WATCH TRAILER</span></a>
-
-						<a href="#" class="button shade"><i class="fa fa-ticket fa-2x fa-fw"></i><span style="width:150px; margin-left:10px;	display:inline-block">BOOK TICKET</span></a>
-						
-				</div> -->
-				<!-- <img src="images/b2.jpg" width="100%" height="100%"> -->
-				<?php include('slider.php'); ?>
-			</div>
-		  <div class="clearfix"></div>
-			<div class="col-md-12 list">
-				<div class="col-md-2"><img src="images/r1.jpg" ></div>
-				<div class="col-md-2"><img src="images/r2.jpg" ></div>
-				<div class="col-md-2"><img src="images/r3.jpg" ></div>
-				<div class="col-md-2"><img src="images/r4.jpg" ></div>
-				<div class="col-md-2"><img src="images/r5.jpg" ></div>
-				<div class="col-md-2"><img src="images/r6.jpg" ></div>
-			</div>
-
-				<div class="clearfix"></div>
-
-			<div class="col-md-6 mid-left">
-				<p>Don't be late,</p>
-				<h1>Book your ticket now!</h1>
-				<p>Easy, simple & fast.</p>
-
-
-				<a href="" class="button shade1"><i class="fa fa-ticket fa-2x fa-fw"></i> <span>BOOK TICKET</span></a>
-			</div>
-
-			<div class="col-md-6 mid-right">
-				<h2>Categories</h2>
-				<div class="col-md-6"><a href=""><div><p>Romantic</p></div></a></div>
-				<div class="col-md-6"><a href=""><div><p>Comedy</p></div></a></div>
-				<div class="clearfix"></div>
-				<div class="col-md-6"><a href=""><div><p>Action</p></div></a></div>
-				<div class="col-md-6"><a href=""><div><p>Anime</p></div></a></div>
-			</div>
-
+	<div class="col-md-6 col-md-offset-3 login">
+		<div class="col-md-4 text-center">
+			<i class="fa fa-user-secret fa-5x"></i>
+			<h4>OHM CINEMA PANEL</h4>
+			<h1>SIGN IN</h1>
 			
+		</div>
+		<div class="col-md-8 text-center">
+            <form action="verify.php" method="post">
+				<p><input name="username" type="text" placeholder="User Name" required></p>
+				<p><input name="password" type="password" placeholder="Password" required></p>
+				<p><input type="submit" value="Sign In" class="button"></p>
+			</form>
+			<p>
+				<?php
+					if(isset($_GET['error']))
+					{
+						echo "Incorrect username or password!";
+					}
+				?>
+			</p>
+		</div>
+
+	</div>
 
 
-
-<?php include('footer.php'); ?>
+    <p class="footer"></p>
+</body>
+</html>
