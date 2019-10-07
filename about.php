eeb652a85171638d9d6eb351c4f2a857436f66ec
simrankaur7859 <49976793+simrankaur7859@users.noreply.github.com>
2019-10-08 09:50:58 +1000

    Add files via upload
    about page

---
+++ about.php
@@ -0,0 +1,44 @@
+<?php include('header.php'); ?>
+
+				
+			
+	<div class="col-md-12 " style="margin-top:0px;">
+
+		<div class="col-md-8 col-md-offset-2 feed">
+			
+			<div class="row"><h3>About Us</h3></div>
+			<p><b>Pride Of Sirsa</b></p>
+			
+			<p class="text-justify" style="line-height:30px;">
+				SIRSA, a growing urban centre, has added a multiplex to its entertainment portfolio . The exhibition company, whose newest property is located at hisar road sirsa, now has one more properties across India. The company hopes that sirsa’s reputation as a cultural hub of the hindi & Punjabi film industry will boost the popularity of its new venture.
+
+Pankaj Khemka, Director , says, "we are very pleased to make an entry into the region’s, Cinema has strong traction in Haryana and this property has only reinforced our presence in the state . </p>
+<div class="clearfix"></div>
+
+			<div class="col-md-12  mid-left">
+				<img src="images/about1.jpg" alt="Smiley face" height="220" width="320">
+				<img src="images/about2.jpg" alt="Smiley face" height="220" width="320">
+				<div class="clearfix"></div></div>
+				<p class="text-justify" style="line-height:30px;">
+				it has pioneered the movie – premier culture . with this launch, the audience in sirsa will enjoy the flavor of our warm hospitality".
+
+The multiplex is equipped with its benchmarked technology, lighting and acoustic sound system . in addition, this property offers luxurious recliner seats, a variety of gourmet food , a personalized café service and centralized ticketing with these state- of-the-art amenities and a splendid movie – watching ambience, cinema offers an experience to savour.
+
+The 2 screen multiplex has a total seating capacity of 504, which are divided into: AUDI 1 (289),AUDI 2 (215). Show casting Hindi , Punjabi and English movies in 2 D as well as 3 D , ticket rates range from Rs 70 to 250 varying also with show timing
+
+The box office counter show timings and ticket prices. The cinema is designed with chic and lively colours, and contemporary lighting makes the lobby look vigorous and energetic . with granite & tiles flooring and a special lounge area..
+
+The ceiling of the lounge has a bright, flame – like motif, with red & blue walls, sofas and a floor to compliment it. The entire lounge area reflects an affluent look and enhances the feeling of space. The food court is simple and is aglow in contemporary lighting. With multi-ethnic colored tiles, the floor put a definite spring in your step . the exterior of the multiplex has double – skin cladding on the top with a sky light and elements of glass in a contemporary architecture. 
+
+Concession stands provide a variety of edibles, including gourmet meals and combos. Flavoured popcorn and sandwich are also available. And, oh, there’s also a baggage counter conveniently provided for avid shoppers! 
+				</p>
+			
+			
+			
+			
+			
+		</div>
+
+	</div>
+
+<?php include('footer.php'); ?>
